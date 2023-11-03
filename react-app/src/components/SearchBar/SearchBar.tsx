ab65d42728aaab7eb28d76e373d96296f2d2aed2
Калашников Семен <horselvaren@gmail.com>
2023-11-03 20:05:06 +0600

    refactor: turn class components to functional ones

--- react-app/src/components/SearchBar/SearchBar.tsx
+++ react-app/src/components/SearchBar/SearchBar.tsx
@@ -1,40 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
 
 type SearchBarProps = {
   onSearch: (searchTerm: string) => void;
 };
 
-type SearchBarState = {
-  searchTerm: string;
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState(localStorage.getItem('searchTerm') || '');
+
+  const handleSearch = () => {
+    const trimmedSearchTerm = searchTerm.trim();
+    localStorage.setItem('searchTerm', trimmedSearchTerm);
+    onSearch(trimmedSearchTerm);
+  };
+
+  return (
+    <div className={styles.searchBar}>
+      <input
+        className={styles.searchBarInput}
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button className={styles.searchBarButton} onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
 };
-
-class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
-  state: SearchBarState = {
-    searchTerm: localStorage.getItem('searchTerm') || '',
-  };
-
-  handleSearch = () => {
-    const searchTerm = this.state.searchTerm.trim();
-    localStorage.setItem('searchTerm', searchTerm);
-    this.props.onSearch(searchTerm);
-  };
-
-  render() {
-    return (
-      <div className={styles.searchBar}>
-        <input
-          className={styles.searchBarInput}
-          type="text"
-          value={this.state.searchTerm}
-          onChange={(e) => this.setState({ searchTerm: e.target.value })}
-        />
-        <button className={styles.searchBarButton} onClick={this.handleSearch}>
-          Search
-        </button>
-      </div>
-    );
-  }
-}
 
 export default SearchBar;
--- react-app/src/pages/SearchPage/SearchPage.tsx
+++ react-app/src/pages/SearchPage/SearchPage.tsx
@@ -1,80 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './SearchPage.module.css';
-
 import SearchBar from '../../components/SearchBar';
 import Results from '../../components/Results';
-
 import { Person } from '../../types';
 
-type SearchPageState = {
-  searchResults: Person[];
-  error: boolean;
-  testError: boolean;
-  isLoading: boolean;
-};
+const SearchPage: React.FC = () => {
+  const [searchResults, setSearchResults] = useState<Person[]>([]);
+  const [error, setError] = useState(false);
+  const [testError, setTestError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-class SearchPage extends React.Component<
-  Record<string, never>,
-  SearchPageState
-> {
-  state: SearchPageState = {
-    searchResults: [],
-    error: false,
-    testError: false,
-    isLoading: false,
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     const initialSearchTerm = localStorage.getItem('searchTerm') || '';
-    this.handleSearch(initialSearchTerm);
-  }
+    handleSearch(initialSearchTerm);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  testError = () => {
-    this.setState({ testError: true });
+  const testErrorFunction = () => {
+    setTestError(true);
   };
 
-  handleSearch = async (searchTerm: string) => {
-    this.setState({ isLoading: true });
-
+  const handleSearch = async (searchTerm: string) => {
+    setIsLoading(true);
     try {
       const response = await fetch(
         `https://swapi.dev/api/people/?search=${searchTerm}`
       );
       const data = (await response.json()) as { results: Person[] };
-      this.setState({ searchResults: data.results, isLoading: false });
+      setSearchResults(data.results);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
-      this.setState({ error: true, isLoading: false });
+      setError(true);
+      setIsLoading(false);
     }
   };
 
-  render() {
-    if (this.state.testError) {
-      throw new Error('Test error!');
-    }
-
-    return (
-      <>
-        <div className={styles.searchPage}>
-          <div className={styles.searchPageRow}>
-            <SearchBar onSearch={this.handleSearch} />
-          </div>
-
-          <div className={styles.searchPageRow}>
-            {this.state.isLoading ? (
-              <div className={styles.loader}>Loading...</div>
-            ) : (
-              <Results data={this.state.searchResults} />
-            )}
-          </div>
-
-          <button className={styles.errorButton} onClick={this.testError}>
-            Test Error
-          </button>
-        </div>
-      </>
-    );
+  if (testError) {
+    throw new Error('Test error!');
   }
-}
+
+  return (
+    <>
+      <div className={styles.searchPage}>
+        <div className={styles.searchPageRow}>
+          <SearchBar onSearch={handleSearch} />
+        </div>
+
+        <div className={styles.searchPageRow}>
+          {isLoading ? (
+            <div className={styles.loader}>Loading...</div>
+          ) : (
+            <Results data={searchResults} />
+          )}
+        </div>
+
+        <button className={styles.errorButton} onClick={testErrorFunction}>
+          Test Error
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default SearchPage;
