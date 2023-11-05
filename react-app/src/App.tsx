5465865a2e75507b6dceabad14178f2b2d851b15
Калашников Семен <horselvaren@gmail.com>
2023-11-05 23:53:44 +0600

    refactor: update structure

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import SearchPage from './pages/SearchPage';
 import DetailPage from './pages/DetailPage';
 import ErrorBoundary from './components/ErrorBoundary';
 
-function App() {
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
   return (
-    <ErrorBoundary>
-      <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/people/:id" element={<DetailPage />} />
-      </Routes>
-    </ErrorBoundary>
+    <React.StrictMode>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path=":page" element={<SearchPage />}>
+              <Route path="details/:details" element={<DetailPage />} />
+            </Route>
+          </Routes>
+        </ErrorBoundary>
+      </BrowserRouter>
+    </React.StrictMode>
   );
-}
+};
 
 export default App;
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,25 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
-import SearchPage from './pages/SearchPage';
-import DetailPage from './pages/DetailPage';
-import ErrorBoundary from './components/ErrorBoundary';
+import App from './App';
 
-import { Routes, Route } from 'react-router-dom';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <Routes>
-          <Route path="/" element={<SearchPage />} />
-          <Route path=":page" element={<SearchPage />}>
-            <Route path="details/:details" element={<DetailPage />} />
-          </Route>
-        </Routes>
-      </ErrorBoundary>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
