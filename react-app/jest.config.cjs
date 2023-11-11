c585eaa3df33707a72c7ddbd1de6387055496b41
Калашников Семен <horselvaren@gmail.com>
2023-11-12 02:55:19 +0600

    chore: add jest-fetch-mock

--- react-app/jest.config.cjs
+++ react-app/jest.config.cjs
@@ -1,12 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom',
+    'jest-fetch-mock'
+  ],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./styleMock.js')
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest'
-  }
+  },
 };
